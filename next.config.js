2dc8767363365af92d16ce223285bfffe004b119
skylinebin <xyliubin@outlook.com>
2023-08-19 17:35:44 +0800

    update config of pages
    remove output dir

--- next.config.js
+++ next.config.js
@@ -7,13 +7,6 @@ module.exports = {
   output: "export",
 
   /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/bin-tools",
-
-  /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
