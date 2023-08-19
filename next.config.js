0af170c82d2967838b32287346263f9401368128
skylinebin <xyliubin@outlook.com>
2023-08-19 17:01:14 +0800

    feat: update base path

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/bin-tools",
 
   /**
    * Disable server-based image optimization. Next.js does not support
